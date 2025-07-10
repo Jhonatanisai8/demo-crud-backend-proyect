e534a2cee8c91477bdef3e5786e28c77cc147cd2
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-09 23:02:06 -0500

    init

---
+++ src/main/java/com/isai/demo_crud_backend_proyect/DemoCrudBackendProyectApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_crud_backend_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudBackendProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrudBackendProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_crud_backend_proyect/DemoCrudBackendProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_crud_backend_proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudBackendProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
