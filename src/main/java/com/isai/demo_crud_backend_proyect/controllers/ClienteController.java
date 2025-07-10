b0eeb79102d68954da950383e7b0d404fbd2bc62
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-10 00:02:38 -0500

    agregando el codigo de respuesta en el metodo guardar cliente que esta en el controlador

--- src/main/java/com/isai/demo_crud_backend_proyect/controllers/ClienteController.java
+++ src/main/java/com/isai/demo_crud_backend_proyect/controllers/ClienteController.java
@@ -1,8 +1,10 @@
 package com.isai.demo_crud_backend_proyect.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isai.demo_crud_backend_proyect.models.entitis.Cliente;
@@ -18,6 +20,7 @@ public class ClienteController {
     private final ClienteService clienteService;
 
     @RequestMapping(path = "/clientes", method = RequestMethod.POST)
+    @ResponseStatus(code = HttpStatus.OK)
     public Cliente guardarCliente(@RequestBody Cliente cliente) {
         return clienteService.guardarCliente(cliente);
     }
