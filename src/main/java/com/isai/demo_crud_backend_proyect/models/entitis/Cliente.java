1c4fa9000b11f571d9a5885f1ca369da2b7dc441
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-09 23:29:35 -0500

    creacion de la entidad Cliente

---
+++ src/main/java/com/isai/demo_crud_backend_proyect/models/entitis/Cliente.java
@@ -0,0 +1,35 @@
+package com.isai.demo_crud_backend_proyect.models.entitis;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cliente")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Integer idCliente;
+
+    @Column(length = 20, nullable = false)
+    private String nombres;
+    
+    @Column(length = 20, nullable = false)
+    private String apellidos;
+    
+    @Column(length = 50, nullable = false)
+    private String email;
+}
