97f5434c28519c2cfd6cdfd22357eb143dafa3d8
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-07-09 23:56:51 -0500

    controllador para el cliente, implementacion del emtodo para crear

---
+++ src/main/java/com/isai/demo_crud_backend_proyect/controllers/ClienteController.java
@@ -0,0 +1,24 @@
+package com.isai.demo_crud_backend_proyect.controllers;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.isai.demo_crud_backend_proyect.models.entitis.Cliente;
+import com.isai.demo_crud_backend_proyect.service.ClienteService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1")
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    @RequestMapping(path = "/clientes", method = RequestMethod.POST)
+    public Cliente guardarCliente(@RequestBody Cliente cliente) {
+        return clienteService.guardarCliente(cliente);
+    }
+}
